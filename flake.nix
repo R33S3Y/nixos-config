f5a6467c90e2a27e1e757d300d17e81406fbe16d
R33S3Y <r3es3y@gmail.com>
2024-12-22 21:38:40 +1300

    update nix version

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "NixOS Configuration for Diamond and Amethyst";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
   };
 
   outputs = { self, nixpkgs, home-manager, ... }: 
