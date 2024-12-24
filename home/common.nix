fc710ae7a9ee335491e5de39ebef6b7061e9d487
R33S3Y <r3es3y@gmail.com>
2024-12-25 07:11:18 +1300

    Test

--- home/common.nix
+++ home/common.nix
@@ -4,10 +4,13 @@
   programs.home-manager.enable = true;
   programs.git.enable = true;
 
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "24.11";
+  home.username = "reese";
+
   # Nicely reload system units when changing configs
   #systemd.user.startServices = "sd-switch";
 
-  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  home.stateVersion = "24.11";
+  
 }
 
