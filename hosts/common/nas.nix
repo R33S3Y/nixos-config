17bbf48f609813391dd3f5e030317fa4ea9b8f41
R33S3Y <r3es3y@gmail.com>
2024-12-29 10:48:47 +1300

    ???

--- hosts/common/nas.nix
+++ hosts/common/nas.nix
@@ -6,13 +6,9 @@
     cifs-utils # For SMB/CIFS
   ];
   
-  fileSystems."/home/reese/lapus_lazuli" = {
+  fileSystems."/mnt/lapus_lazuli" = {
     device = "//192.168.1.253/lapus_lazuli";
     fsType = "cifs";
     options = [ "credentials=/etc/nas_credentials" "uid=reese" "gid=users" "iocharset=utf8" ];
-    preMountCommands = ''
-      mkdir -p /home/reese/lapus_lazuli
-      chown reese:users /home/reese/lapus_lazuli
-    '';
   };
 }
