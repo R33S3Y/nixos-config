00b12735e3af05f68015e83843ece8b0c986a6d5
R33S3Y <r3es3y@gmail.com>
2024-12-28 14:47:18 +1300

    boot???

--- hosts/common/boot.nix
+++ hosts/common/boot.nix
@@ -16,14 +16,14 @@
   boot.kernelParams = [
       "quiet"
       "splash"
-      #"boot.shell_on_fail"
-      #"loglevel=3"
-      #"rd.systemd.show_status=false"
-      #"rd.udev.log_level=3"
-      #"udev.log_priority=3"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
     ];
     # Hide the OS choice for bootloaders.
     # It's still possible to open the bootloader list by pressing any key
     # It will just not appear on screen unless a key is pressed
-  boot.loader.timeout = 0;
+  boot.loader.timeout = 1;
 }
