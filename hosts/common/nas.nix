263de96635a322b6d0bd77e0fa81d8b35d17f747
R33S3Y <r3es3y@gmail.com>
2024-12-29 10:42:42 +1300

    mount

--- hosts/common/nas.nix
+++ hosts/common/nas.nix
@@ -6,17 +6,13 @@
     cifs-utils # For SMB/CIFS
   ];
   
-  system.activationScripts.customCommand = {
-    text = ''
-      echo "Running custom command..."
-      mkdir -p /home/reese/lapus_lazuli
-      chown reese:users /home/reese/lapus_lazuli
-    '';
-  };
-
   fileSystems."/home/reese/lapus_lazuli" = {
     device = "//192.168.1.253/lapus_lazuli";
     fsType = "cifs";
     options = [ "credentials=/etc/nas_credentials" "uid=reese" "gid=users" "iocharset=utf8" ];
+    preMountCommands = ''
+      mkdir -p /home/reese/lapus_lazuli
+      chown reese:users /home/reese/lapus_lazuli
+    '';
   };
 }
