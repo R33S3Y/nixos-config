83c6ba91d13e13e680978df5a20c0847219eb52a
R33S3Y <r3es3y@gmail.com>
2024-12-28 14:16:42 +1300

    pretty boots fixed??

--- hosts/common/boot.nix
+++ hosts/common/boot.nix
@@ -6,6 +6,24 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  services.plymouth.enable = true; # boot mode img software
-  boot.kernelParams = [ "quiet" "splash" ]; # actual make it show up
+
+  # make boots pretty
+  boot.plymouth.enable = true; # themes are handled by stylix
+  
+  # Enable "Silent Boot" (disable loging)
+  boot.consoleLogLevel = 0;
+  boot.initrd.verbose = false;
+  boot.kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+    # Hide the OS choice for bootloaders.
+    # It's still possible to open the bootloader list by pressing any key
+    # It will just not appear on screen unless a key is pressed
+  boot.loader.timeout = 0;
 }
