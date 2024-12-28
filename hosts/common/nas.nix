440fd8ed811648882f6d9f1cad55af0a6f86b94b
R33S3Y <r3es3y@gmail.com>
2024-12-29 10:36:11 +1300

    file fix

--- hosts/common/nas.nix
+++ hosts/common/nas.nix
@@ -6,6 +6,11 @@
     cifs-utils # For SMB/CIFS
   ];
   
+  systemd.tmpfiles.rules = [
+    # Ensure the directory exists with the correct ownership and permissions
+    "d /home/reese/lapus_lazuli 0755 reese users -"
+  ];
+
   fileSystems."/home/reese/lapus_lazuli" = {
     device = "//192.168.1.253/lapus_lazuli";
     fsType = "cifs";
