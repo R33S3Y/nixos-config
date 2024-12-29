fb5e9dd0b7df3274a52cb51246a90985b3c84707
R33S3Y <r3es3y@gmail.com>
2024-12-29 17:37:52 +1300

    logo no spin

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -9,6 +9,43 @@
 
       "$mainMod" = "SUPER";
 
+      general  = {
+        # See https://wiki.hyprland.org/Configuring/Variables/ for more
+
+        gaps_in = "5";
+        gaps_out = "20";
+        border_size = "2";
+        #col.active_border = "rgba(c18fb3ff) rgba(c18fb3ff) 45deg"
+        #col.inactive_border = "rgba(57526cff)"
+
+        layout = "dwindle";
+
+        # Please see https://wiki.hyprland.org/Configuring/Tearing/ before you turn this on
+        # allow_tearing = false
+      };
+
+      decoration {
+        # See https://wiki.hyprland.org/Configuring/Variables/ for more
+
+        rounding = "10";
+        
+        blur = {
+          enabled = "true";
+          size = "7";
+          passes = "1";
+        };
+
+        shadow {
+          enabled = "true";
+          range = "4";
+          render_power = "3";
+          color = "rgba(1a1a1aee)";
+        };
+
+        active_opacity = "1";
+        inactive_opacity = "0.6";
+      };
+
       bind = [
         # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
         "$mainMod, Q, exec, $terminal"
@@ -59,8 +96,8 @@
         "$mainMod, mouse_up, workspace, e+1"
 
         # Move/resize windows with mainMod + LMB/RMB and dragging
-        "$mainMod, mouse:272, movewindow,"
-        "$mainMod, mouse:273, resizewindow,"
+        "$mainMod, mouse:272, movewindow"
+        "$mainMod, mouse:273, resizewindow"
 
         # Screenshot a window
         "$mainMod SHIFT, PRINT, exec, hyprshot -m window"
--- hosts/common/boot.nix
+++ hosts/common/boot.nix
@@ -9,21 +9,7 @@
 
   # make boots pretty
   boot.plymouth.enable = true; # themes are handled by stylix
+  stylix.targets.plymouth.logoAnimated = false;
   
-  # Enable "Silent Boot" (disable loging)
-  # boot.consoleLogLevel = 0;
-  # boot.initrd.verbose = false;
-  # boot.kernelParams = [
-  #    #"quiet"
-  #    "splash"
-  #    "boot.shell_on_fail"
-  #    "loglevel=3"
-  #    "rd.systemd.show_status=false"
-  #    "rd.udev.log_level=3"
-  #    "udev.log_priority=3"
-  #  ];
-    # Hide the OS choice for bootloaders.
-    # It's still possible to open the bootloader list by pressing any key
-    # It will just not appear on screen unless a key is pressed
   boot.loader.timeout = 0;
 }
