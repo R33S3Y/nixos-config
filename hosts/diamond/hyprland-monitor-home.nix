714df0165e77e6a8c146174fc9baf13d0a2bb733
R33S3Y <r3es3y@gmail.com>
2024-12-30 18:20:33 +1300

    changes

--- hosts/diamond/hyprland-monitor-home.nix
+++ hosts/diamond/hyprland-monitor-home.nix
@@ -5,7 +5,7 @@
     settings = {
       monitor = [
         "HDMI-A-1, 1920x1080@60, 0x0, 1"
-        "DP-3, 2560x1440@165, 1920x0, 2, transform, 3"
+        "DP-3, 2560x1440@165, 1920x-200, 2, transform, 3"
       ];
     };
   };
