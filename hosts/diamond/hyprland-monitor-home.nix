103987b47fcf806cdb6138ef13b5a74213571f05
R33S3Y <r3es3y@gmail.com>
2024-12-30 18:17:59 +1300

    ??

--- hosts/diamond/hyprland-monitor-home.nix
+++ hosts/diamond/hyprland-monitor-home.nix
@@ -4,8 +4,8 @@
     enable = true;
     settings = {
       monitor = [
-        "HDMI-A-1, 1920x1080@60, -1920x-1080, 1"
-        "DP-3, 2560x1440@165, 0x-2560, 2, transform, 3"
+        "HDMI-A-1, 1920x1080@60, 0x0, 1"
+        "DP-3, 2560x1440@165, 1920x0, 2, transform, 3"
       ];
     };
   };
