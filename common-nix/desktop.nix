4552efee3cea8b64b690231c5c2f725008b26d38
R33S3Y <r3es3y@gmail.com>
2025-02-01 17:48:11 +1300

    hyprlock

--- common-nix/desktop.nix
+++ common-nix/desktop.nix
@@ -7,6 +7,7 @@
   # services.desktopManager.plasma6.enable = true;
 
   programs.hyprland.enable = true;
+  security.pam.services.hyprlock = {};
 
   environment.systemPackages = with pkgs; [
     kitty # cmd
