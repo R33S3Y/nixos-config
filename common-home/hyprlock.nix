9b80ab63818074b0a86f67e47696df895ba5e164
R33S3Y <r3es3y@gmail.com>
2025-02-01 17:55:01 +1300

    test

--- common-home/hyprlock.nix
+++ common-home/hyprlock.nix
@@ -12,7 +12,7 @@
 
       background = [
         {
-          path = "${config.stylix.image}";
+          #path = "${config.stylix.image}";
           blur_passes = 3;
           blur_size = 8;
         }
