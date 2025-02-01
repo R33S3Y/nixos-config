7af3cb4e5d7e5df95a8059189654eebac21debe9
R33S3Y <r3es3y@gmail.com>
2025-02-01 16:55:01 +1300

    bsm?

--- hosts/diamond/bs-manager.nix
+++ hosts/diamond/bs-manager.nix
@@ -4,8 +4,17 @@
 {
   services.flatpak.enable = true;
   system.activationScripts.bs-manager.text = ''
+    export PATH=${pkgs.flatpak}/bin:$PATH
+
+    # Ensure Flathub is added
     flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    flatpak install https://github.com/Zagrios/bs-manager/releases/download/v1.5.1/BSManager-1.5.1-x86_64.flatpak
+
+    # Check if BSManager is already installed
+    if ! flatpak list | grep -q 'BSManager'; then
+      flatpak install -y https://github.com/Zagrios/bs-manager/releases/download/v1.5.1/BSManager-1.5.1-x86_64.flatpak
+    else
+      echo "BSManager is already installed. Skipping..."
+    fi
   '';
   
 }
