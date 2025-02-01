03e54400ad42f0f27c128e94c0dcafb0417611aa
R33S3Y <r3es3y@gmail.com>
2025-02-01 19:59:07 +1300

    ?

--- common-home/hyprlock.nix
+++ common-home/hyprlock.nix
@@ -14,7 +14,10 @@ in {
         no_fade_in = false;
         disable_loading_bar = false;
       };
-
+      auth {
+        method = pam;
+        prompt = "Password: ";  # Customize the prompt text if desired
+      };
       # BACKGROUND
       background = {
         monitor = "";
