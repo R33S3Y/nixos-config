abb3cbdcc25594c48d6906e58eb0e66dce37fe2f
R33S3Y <r3es3y@gmail.com>
2025-03-09 11:47:44 +1300

    2025-03-09 - Attempting Build for gen: 164

--- users/users-nix.nix
+++ users/users-nix.nix
@@ -2,7 +2,7 @@
 { config, pkgs, ... }:
 
 {
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # Define a user account. Don't forget to set a password with ‘passwd’. 
   users.users.reese = {
     isNormalUser = true;
     description = "reese";
