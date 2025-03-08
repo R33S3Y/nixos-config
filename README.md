21579768a0764620a2a2b317290040489833f395
R33S3Y <r3es3y@gmail.com>
2025-03-09 11:02:07 +1300

    2025-03-09 - Attempting Build for gen: 159

--- README.md
+++ README.md
@@ -6,4 +6,6 @@ This is the repository where I keep my nixos config.
 
 This repository is made public purely for my convenience and I will not bother helping any one with it in any form. Of course it is open source so can go crazy and do whatever you want to do with it, just know that I will not be helping you in any way.
 
-Please note that for some reason it seems that you need a 100% clean git repo for 1st build.
+Please note that for some reason it seems that you need to commit your changes before it is will to reconize the existance of new files
+
+Also attempted builds can be assumed successful by the fact that you can see them as I only push on success nowdays
--- optional/strawberry/bind-home.nix
+++ optional/strawberry/bind-home.nix
@@ -4,6 +4,7 @@
     enable = true;
     settings = {
       "$mainMod" = "SUPER";
+      "$player" = "strawberry";
 
       exec-once = [
         "sh -c 'sleep 1 && bluetoothctl connect C4:77:64:6C:56:95'" # connect to my Galaxy Buds FE
@@ -12,7 +13,15 @@
 
       bind = [
         # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
-        #"$mainMod, Q, exec, $terminal"
+
+        # These map the deacated audio pause and play as well as volume to strawberry
+        ", XF86AudioPrev, exec, $player --previous"
+        ", XF86AudioPlay, exec, $player --play-pause"
+        ", XF86AudioNext, exec, $player --next"
+
+        ", XF86AudioMute, exec, $player --volume 0"
+        ", XF86AudioLowerVolume, exec, $player --volume-decrease-by 5"
+        ", XF86AudioRasieVolume, exec, $player --volume-increase-by 5"
       ];
     };
   };
