3e4269627ba6e4d35e1f117789d0f4d1cf9b88d8
R33S3Y <r3es3y@gmail.com>
2025-03-09 11:26:22 +1300

    2025-03-09 - Attempting Build for gen: 163

--- optional/vscode/settings-home.nix
+++ optional/vscode/settings-home.nix
@@ -2,6 +2,7 @@
 
 {
   stylix.targets.vscode.enable = true;
+  config.stylix.targets.vscode.profileNames = [ config.var.username ];
   programs.vscode = {
     enable = true;
     profiles.default = {
