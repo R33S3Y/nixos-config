822e3f9f48161a12b4915896ce0f0f3dafd69a8a
R33S3Y <r3es3y@gmail.com>
2025-03-27 22:27:22 +1300

    2025-03-27 - Attempting Build for gen: 195

--- core/hyprland/style-home.nix
+++ core/hyprland/style-home.nix
@@ -2,11 +2,17 @@
 { 
 
   home.pointerCursor = {
-    name = "BreezeX-RosePine-Linux"; #is folder name
+    name = "BreezeX-RosePine-Linux"; # Cursor name is the folder name of your choice in /nix/store/{{cursor Theme}}/share/icons
     package = pkgs.rose-pine-cursor;
     size = 24;
-    #gtk.enable = true;
-    #x11.enable = true;
+
+    gtk.enable = true;
+    x11.enable = true;
+    
+    # Pointer is still not 100% consisant bettween apps, desktop and the phase of the moon. (when pixel peeping)
+    # But it's close enough that I dont care.
+    # That and I'm pretty sure that it's down to things like diffs with gtk and qt or X11 scaling or something like that
+
   };
 
   wayland.windowManager.hyprland = {
