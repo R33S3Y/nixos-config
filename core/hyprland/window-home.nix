4f7b5631db7c8161ad3e434c79ab52cf45487846
R33S3Y <r3es3y@gmail.com>
2025-04-28 21:03:09 +1200

    2025-04-28 - Attempting Build for gen: 253

---
+++ core/hyprland/window-home.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+{
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      windowrulev2 = [
+        "nofullscreenrequest,class:^(steam_app_),title:^(?!SteamVR)"
+        "noinitialfullscreen,class:^(steam_app_),title:^(?!SteamVR)"
+        "tile,class:^(steam_app_),title:^(?!SteamVR)"
+      ];
+    };
+  };
+}
