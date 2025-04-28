ef2025eb5f87fb6a6e27fee7af697b96a96eb12e
R33S3Y <r3es3y@gmail.com>
2025-04-28 21:19:27 +1200

    2025-04-28 - Attempting Build for gen: 258

--- core/hyprland/window-home.nix
+++ core/hyprland/window-home.nix
@@ -6,8 +6,7 @@
       "windowrulev2" = [
         #"nofullscreenrequest,class:^(steam_app_),title:^(?!SteamVR)"
         #"noinitialfullscreen,class:^(steam_app_),title:^(?!SteamVR)"
-        "unset, class:^(steam_app_[0-9]+)$"
-        "nofullscreen, class:^(steam_app_[0-9]+)$"
+        "tile, title:^(steam_app_)"
       ];
     };
   };
