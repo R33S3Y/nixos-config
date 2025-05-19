6ac08f053aba6b18a05daae265616af8c9ab4e1a
R33S3Y <r3es3y@gmail.com>
2025-05-20 10:54:45 +1200

    2025-05-20 - Attempting Build for gen: 306

--- core/boot/style-nix.nix
+++ core/boot/style-nix.nix
@@ -119,7 +119,7 @@ in
   };
 
 
-  boot.plymouth.enable = true; # themes are handled by stylix
+  config.boot.plymouth.enable = true; # themes are handled by stylix
   stylix.targets.plymouth.logoAnimated = false;
 
   stylix.targets.grub.enable = false;
