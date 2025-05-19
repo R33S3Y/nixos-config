65498e09305f6eed54df04f96bebe293a2789d46
R33S3Y <r3es3y@gmail.com>
2025-05-20 07:42:08 +1200

    2025-05-20 - Attempting Build for gen: 300

--- core/boot/grub-nix.nix
+++ core/boot/grub-nix.nix
@@ -4,7 +4,7 @@
 {
   # Bootloader.
   boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/boot";
+  boot.loader.grub.device = "/dev/disk/by-uuid/C197-7115";
   boot.loader.grub.useOSProber = true;
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
