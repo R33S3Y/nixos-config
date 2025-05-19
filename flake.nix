9557795cbde9a3221fc3cd551b2ea303f1ab2174
R33S3Y <r3es3y@gmail.com>
2025-05-19 19:51:03 +1200

    2025-05-19 - Attempting Build for gen: 289

--- flake.nix
+++ flake.nix
@@ -45,4 +45,29 @@
       };
     };
   };
+
+  deploy.nodes = {
+    diamond = {
+      hostname = "diamond";  # Change to your real hostname or IP
+      sshUser = "reese";            # Or your user with NixOS rebuild rights
+      profiles.system = {
+        user = "reese";
+        path = ./#diamond;
+      };
+    };
+
+    obsidian = {
+      hostname = "obsidian"; # Change this too
+      sshUser = "reese";
+      profiles.system = {
+        user = "reese";
+        path = ./#obsidian;
+      };
+    };
+  };
+
+  deploy.defaults = {
+    magicRollback = true;
+    autoRollback = true;
+  };
 }
