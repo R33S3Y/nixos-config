3c8c561c7994f212f6ff786e1aadd54c7340b245
R33S3Y <r3es3y@gmail.com>
2025-05-20 07:40:41 +1200

    2025-05-20 - Attempting Build for gen: 299

--- core/boot/grub-nix.nix
+++ core/boot/grub-nix.nix
@@ -4,7 +4,7 @@
 {
   # Bootloader.
   boot.loader.grub.enable = true;
-  #boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.device = "/boot";
   boot.loader.grub.useOSProber = true;
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
