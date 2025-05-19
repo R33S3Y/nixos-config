fd45bcc61d45b48634a38dc861c052a2716f732f
R33S3Y <r3es3y@gmail.com>
2025-05-20 09:23:38 +1200

    2025-05-20 - Attempting Build for gen: 304

--- core/boot/style-nix.nix
+++ core/boot/style-nix.nix
@@ -6,7 +6,7 @@
   boot.plymouth.enable = true; # themes are handled by stylix
   stylix.targets.plymouth.logoAnimated = false;
 
-  stylix.grub.enable = false;
+  stylix.targets.grub.enable = false;
   boot.loader.grub.splashImage = config.stylix.image;
   boot.loader.grub.theme = "${pkgs.libsForQt5.breeze-grub}/grub/themes/breeze";
   
