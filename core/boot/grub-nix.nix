724c9e98f55e7dc71720e338e21de3652e978a56
R33S3Y <r3es3y@gmail.com>
2025-05-20 07:51:02 +1200

    2025-05-20 - Attempting Build for gen: 302

--- core/boot/grub-nix.nix
+++ core/boot/grub-nix.nix
@@ -4,8 +4,9 @@
 {
   # Bootloader.
   boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.device = "nodev";
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
 }
