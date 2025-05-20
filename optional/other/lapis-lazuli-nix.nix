13d4f29816de054fab700fcd43d4b7017c787fbf
R33S3Y <r3es3y@gmail.com>
2025-05-20 17:29:11 +1200

    2025-05-20 - Attempting Build for gen: 323

--- optional/other/lapis-lazuli-nix.nix
+++ optional/other/lapis-lazuli-nix.nix
@@ -11,7 +11,7 @@
     fsType = "cifs";
     options = [ "credentials=${config.var.lapisLazuli.credentials}" "uid=${config.var.username}" "gid=users" "iocharset=utf8" ];
   };
-
+ 
   systemd.tmpfiles.rules = [
     "d ${config.var.lapisLazuli.mount} 0755 ${config.var.username} users - -"
   ];
