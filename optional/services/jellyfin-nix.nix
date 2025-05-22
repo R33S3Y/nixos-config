145586719b3180299965ec588cde478f8e363266
R33S3Y <r3es3y@gmail.com>
2025-05-22 20:16:09 +1200

    2025-05-22 - Attempting Build for gen: 328

--- optional/services/jellyfin-nix.nix
+++ optional/services/jellyfin-nix.nix
@@ -14,4 +14,11 @@
   };
   networking.firewall.allowedUDPPorts = [ 1900 7359 ];
   networking.firewall.allowedTCPPorts = [ 8096 8920 ];
+
+  systemd.tmpfiles.rules = [
+    "d ${config.services.jellyfin.dataDir} 0755 ${config.services.jellyfin.users} users - -"
+    "d ${config.services.jellyfin.configDir} 0755 ${config.services.jellyfin.users} users - -"
+    "d ${config.services.jellyfin.logDir} 0755 ${config.services.jellyfin.users} users - -"
+    "d ${config.services.jellyfin.cacheDir} 0755 ${config.services.jellyfin.users} users - -"
+  ];
 }
