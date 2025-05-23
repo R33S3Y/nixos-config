023b421a096babb9a2bc648d241cccc6138a2eea
R33S3Y <r3es3y@gmail.com>
2025-05-23 23:36:03 +1200

    2025-05-23 - Attempting Build for gen: 337

--- core/kitty/bind-home.nix
+++ core/kitty/bind-home.nix
@@ -7,7 +7,7 @@
       "ctrl+c" = "copy_to_clipboard";
       "ctrl+v" = "paste_from_clipboard";
       "ctrl+shift+c" = ''send_text all \x03'';
-      "escape" = "send_text \x03\\";
+      "escape" = ''send_text all \x03'';
     };
   };
 }
