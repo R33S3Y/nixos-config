da172f8bd28f94a6ba4c00e3e633d670edc5fd53
R33S3Y <r3es3y@gmail.com>
2025-05-23 23:28:21 +1200

    2025-05-23 - Attempting Build for gen: 335

--- core/kitty/bind-home.nix
+++ core/kitty/bind-home.nix
@@ -6,7 +6,8 @@
     keybindings = {
       "ctrl+c" = "copy_to_clipboard";
       "ctrl+v" = "paste_from_clipboard";
-      "ctrl+shift+c" = "send_text all \x03\\";
+      "ctrl+shift+c" = "send_text \x03";
+      "escape" = "send_text \x03\\";
     };
   };
 }
