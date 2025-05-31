656d289494e55717f06d9f407335834b1e3a38f8
R33S3Y <r3es3y@gmail.com>
2025-06-01 11:19:38 +1200

    2025-06-01 - Attempting Build for gen: 343

--- optional/services/jellyfin-nix.nix
+++ optional/services/jellyfin-nix.nix
@@ -4,15 +4,6 @@
 {
   services.jellyfin = {
     enable = true;
-
-    #user = "reese";
-    group = "users";
-
-    dataDir = "/var/lib/jellyfin";
-    cacheDir = "/var/cache/jellyfin";
-
-    logDir = "/var/lib/jellyfin/log";  
-    configDir = "/var/lib/jellyfin/config";
   };
   networking.firewall.allowedUDPPorts = [ 1900 7359 ];
   networking.firewall.allowedTCPPorts = [ 8096 8920 ];
