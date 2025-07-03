621ff19f6f9f8eb30aa8ebb02f5f0bc0e9679ed4
R33S3Y <r3es3y@gmail.com>
2025-07-03 20:22:08 +1200

    2025-07-03 - Attempting Build for gen: 357

--- optional/firefox/settings-home.nix
+++ optional/firefox/settings-home.nix
@@ -1,7 +1,9 @@
 { config, pkgs, inputs, ... }:
 {
   
-  #imports = [ inputs.nur.modules.nixos.default ];
+  imports = [
+    inputs.nur.nixosModules.nur
+  ];
   stylix.targets.firefox.profileNames = ["default"];
   
   programs.firefox = {
