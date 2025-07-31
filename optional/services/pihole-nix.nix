7206a57b0d58cf5f2bd3156572c8d6c337e87ebe
R33S3Y <r3es3y@gmail.com>
2025-07-31 22:06:47 +1200

    2025-07-31 - Attempting Build for gen: 387

--- optional/services/pihole-nix.nix
+++ optional/services/pihole-nix.nix
@@ -13,7 +13,7 @@
     openFirewallDNS = true;
     openFirewallWebserver = true;
     
-    /**
+    
     # Blocklists
     lists = [
       {
@@ -35,7 +35,7 @@
         description = "Steven Black Hosts";
       }
     ];
-    */
+    
     
     # Optional settings
     privacyLevel = 0; # 0 = show everything, increase for more privacy
@@ -43,6 +43,31 @@
       enable = true;
       interval = "weekly";
       age = 7;      # keep logs max 7 days
+    };
+
+    settings = {
+      dns = {
+        domainNeeded = true;
+        expandHosts = true;
+        #listeningMode = "BIND";
+        upstreams = [ "1.1.1.1" ];
+      };
+      dhcp = {
+        active = true;
+        router = "192.168.1.1";
+        start = "192.168.1.2";
+        end = "192.168.1.230";
+        leaseTime = "1d";
+        ipv6 = true;
+        multiDNS = true;
+        rapidCommit = true;
+      };
+      #misc.dnsmasq_lines = [
+        # This DHCP server is the only one on the network
+        #"dhcp-authoritative"
+        # Source: https://data.iana.org/root-anchors/root-anchors.xml
+        #"trust-anchor=.,38696,8,2,683D2D0ACB8C9B712A1948B27F741219298D0A450D612C483AF444A4C0FB2B16"
+      #];
     };
   };
 }
