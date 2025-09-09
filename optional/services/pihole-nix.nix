3aaa1c7b624da1b56f871ba3bb80c27825cb00f6
R33S3Y <r3es3y@gmail.com>
2025-09-10 09:08:46 +1200

    2025-09-10 - Attempting Build for gen: 393

--- optional/services/pihole-nix.nix
+++ optional/services/pihole-nix.nix
@@ -225,7 +225,7 @@
 
       files = {
         pid = "/tmp/pihole-FTL.pid";
-      #  database = "/etc/pihole/pihole-FTL.db";
+        database = "/etc/pihole/pihole-FTL.db";
       #  gravity = "/etc/pihole/gravity.db";
       #  gravity_tmp = "/tmp";
       #  macvendor = "/etc/pihole/macvendor.db";
