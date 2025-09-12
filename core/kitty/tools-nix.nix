51d15a7d41719516350ff5f92f7a1632795b316e
R33S3Y <r3es3y@gmail.com>
2025-09-12 17:22:27 +1200

    2025-09-12 - Attempting Build for gen: 395

--- core/kitty/tools-nix.nix
+++ core/kitty/tools-nix.nix
@@ -5,5 +5,6 @@
   environment.systemPackages = with pkgs; [
     unixtools.netstat #log info about ports with sudo netstat -tulpn
     dig #send test dns trafic Eg dig @1.1.1.1 example.com
+    tcpdump
   ];
 }
