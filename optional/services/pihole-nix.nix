ccb5392fda69b47a635ce629c7ef5f8d0c6eaa93
R33S3Y <r3es3y@gmail.com>
2025-09-14 13:50:05 +1200

    2025-09-14 - Attempting Build for gen: 396

--- optional/services/pihole-nix.nix
+++ optional/services/pihole-nix.nix
@@ -125,7 +125,7 @@
         logging = true;
         ignoreUnknownClients = false;
         hosts = [
-          "7c:f1:7e:b2:70:68,192.168.1.253,lapisLazuli"
+          "7c:f1:7e:b2:70:68,192.168.1.253,lapisLazuli,infinite"
         ];
       };
 
