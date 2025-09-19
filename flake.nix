f793cbda0b7c40664459bd04f5e71bf486587c2b
R33S3Y <r3es3y@gmail.com>
2025-09-19 18:24:16 +1200

    2025-09-19 - Attempting Build for gen: 398

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
           ./hosts/diamond/imports-nix.nix
           inputs.stylix.nixosModules.stylix
           home-manager.nixosModules.home-manager
-          inputs.deploy-rs.modules.nixos.deploy-rs
+          inputs.deploy-rs.activate.nixos
         ];
       };
       obsidian = nixpkgs.lib.nixosSystem {
