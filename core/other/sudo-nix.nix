a6345233b496e1e84d7687721bd6ad7705567d2a
R33S3Y <r3es3y@gmail.com>
2025-09-21 09:08:41 +1200

    2025-09-21 - Attempting Build for gen: 404

--- core/other/sudo-nix.nix
+++ core/other/sudo-nix.nix
@@ -5,7 +5,7 @@
   security.sudo.extraRules = [{
     groups = [ "wheel" ];
     commands = [{
-      command = "/nix/store/*-nixos-rebuild/bin/nixos-rebuild";
+      command = "${pkgs.nixos-rebuild}/bin/nixos-rebuild";
       options = [ "NOPASSWD" ];
     }];
   }];
