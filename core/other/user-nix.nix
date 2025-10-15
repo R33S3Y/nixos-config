7d8d690e73573a9fd4b9544df1f33512b7efaa0a
R33S3Y <r3es3y@gmail.com>
2025-10-15 23:49:10 +1300

    2025-10-15 - Attempting Build for gen: 409

--- core/other/user-nix.nix
+++ core/other/user-nix.nix
@@ -1,10 +1,11 @@
 { config, pkgs, ... }:
-{
+let username = config.var.username;
+in {
   users = {
-    users.reese = {
+    users.${username} = {
       isNormalUser = true;
       uid = 1000;
-      description = "reese";
+      description = "${username}";
       extraGroups = [ "networkmanager" "wheel" ];
     };
   };
