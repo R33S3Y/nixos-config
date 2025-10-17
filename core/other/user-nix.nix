0b6c5f35dbe6e9ec057a30805f5fb331042ef179
R33S3Y <r3es3y@gmail.com>
2025-10-18 09:22:25 +1300

    2025-10-18 - Attempting Build for gen: 409

--- core/other/user-nix.nix
+++ core/other/user-nix.nix
@@ -1,12 +1,19 @@
 { config, pkgs, ... }:
 let username = config.var.username;
 in {
-  users = {
-    users.${username} = {
+  users.users = {
+    ${username} = {
       isNormalUser = true;
       uid = 1000;
       description = "${username}";
       extraGroups = [ "networkmanager" "wheel" ];
+    };
+    rebuild = {
+      isNormalUser = true;
+      uid = 1001;
+      description = "Remote rebuild user";
+      createHome = false; # or true if you want one
+      openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAA..." ];
     };
   };
 }
