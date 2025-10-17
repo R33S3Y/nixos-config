24a1ed70cffb5daa3ea12a5aa7138ebc9d8c2fe8
R33S3Y <r3es3y@gmail.com>
2025-10-18 09:45:31 +1300

    2025-10-18 - Attempting Build for gen: 410

--- core/other/sudo-nix.nix
+++ core/other/sudo-nix.nix
@@ -8,7 +8,7 @@
       command = "/run/current-system/sw/bin/nixos-rebuild";
       options = [ "NOPASSWD" ];
     }];
-  }, {
+  } {
     users = [ "rebuild" ];
     commands = [{
       command = "/run/current-system/sw/bin/nixos-rebuild";
