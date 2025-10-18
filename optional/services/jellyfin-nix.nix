27af01dda7c6a12312634037e00ff36232152198
R33S3Y <r3es3y@gmail.com>
2025-10-18 18:01:36 +1300

    2025-10-18 - Attempting Build for gen: 414

--- optional/services/jellyfin-nix.nix
+++ optional/services/jellyfin-nix.nix
@@ -5,20 +5,10 @@
   services.jellyfin = {
     enable = true;
     openFirewall = true;
-    user = "obsidian"; 
   };
   environment.systemPackages = [
     pkgs.jellyfin
     pkgs.jellyfin-web
     pkgs.jellyfin-ffmpeg
   ];
-
-  users = {
-    users.obsidian = {
-      isNormalUser = true;
-      description = "obsidian";
-      extraGroups = [ "networkmanager" "wheel" ];
-    };
-  };
-}  
- 
+}
