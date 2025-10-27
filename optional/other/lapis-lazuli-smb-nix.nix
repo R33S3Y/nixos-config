854f241239a81ead91d7e750cac60de8a46e03b0
R33S3Y <r3es3y@gmail.com>
2025-10-27 19:32:32 +1300

    2025-10-27 - Attempting Build for gen: 426

--- optional/other/lapis-lazuli-smb-nix.nix
+++ optional/other/lapis-lazuli-smb-nix.nix
@@ -6,7 +6,7 @@
     cifs-utils # For SMB/CIFS
   ];
   
-  fileSystems."${config.var.lapisLazuli.mount}" = {
+  fileSystems."${specialArgs.var.lapisLazuli.mount}" = {
     device = "//192.168.1.253/${config.var.lapisLazuli.share}";
     fsType = "cifs";
     options = [ "credentials=${config.var.lapisLazuli.credentials}" "uid=${config.var.username}" "gid=users" "iocharset=utf8" ];
