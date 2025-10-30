de1036798d2068a6e7a19be98ad9775761b5297d
R33S3Y <r3es3y@gmail.com>
2025-10-30 21:56:40 +1300

    2025-10-30 - Attempting Build for gen: 426

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: 
   let 
-    var = import ./flake/var-nix.nix;
+    var = import ./flake/var-flake.nix;
   in
   {
     nixosConfigurations = {
