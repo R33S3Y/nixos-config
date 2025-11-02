50a3dc0363439fbc12ef4d20669fc00968396207
R33S3Y <r3es3y@gmail.com>
2025-11-02 19:56:59 +1300

    2025-11-02 - Attempting Build for gen: 438

--- hosts/diamond/other/monitor-settings-home.nix
+++ hosts/diamond/other/monitor-settings-home.nix
@@ -5,7 +5,7 @@
     settings = {
       monitor = [
         "HDMI-A-1, 1920x1080@60, 0x0, 1"
-        "DP-3, 2560x1440@165, 1920x-200, 2, transform,3, cm,hdr, bitdepth,10, sdrbrightness,1.2, sdrsaturation,1.1"
+        "DP-3, 2560x1440@165, 1920x-200, 2, transform,3, cm,hdr, bitdepth,10, sdrbrightness,1.2, sdrsaturation,1.3"
         "DP-2, 1920x1080@60, -960x375, 2"
       ];
     };
