d5217cfecc0c4ed754c56275700d3150ee16cdf1
R33S3Y <r3es3y@gmail.com>
2025-11-03 21:11:09 +1300

    2025-11-03 - Attempting Build for gen: 444

--- hosts/diamond/imports-nix.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, specialArgs, ... }:
-{
-
-  home-manager.useGlobalPkgs = true;
-  home-manager.useUserPackages = true;
-
-  home-manager.users."${specialArgs.var.${specialArgs.system}.username}" = import ./imports-home.nix;
-}
