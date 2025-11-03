bb50e61b68ecdcbdca3b4f2a92a7ccddead97576
R33S3Y <r3es3y@gmail.com>
2025-11-03 20:29:56 +1300

    2025-11-03 - Attempting Build for gen: 443

--- core/hyprpanel/style-home.nix
+++ core/hyprpanel/style-home.nix
@@ -1,7 +1,7 @@
 # From: https://github.com/anotherhadi/nixy/tree/main
 # Hyprpanel is the bar on top of the screen
 # Display informations like workspaces, battery, wifi, ...
-{ inputs, config, ... }:
+{ inputs, config, specialArgs, ... }:
 let
   transparentButtons = true;
 
