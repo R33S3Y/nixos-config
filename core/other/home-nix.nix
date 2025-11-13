84e9dbd08358bbbe7d45232a22f8daea1f891ff0
R33S3Y <r3es3y@gmail.com>
2025-11-13 18:40:41 +1300

    2025-11-13 - Attempting Build for gen: 454

--- core/other/home-nix.nix
+++ core/other/home-nix.nix
@@ -1,8 +1,11 @@
 { config, pkgs, specialArgs, ... }:
+let
+  username = specialArgs.var.${specialArgs.system}.username;
+in
 {
 
   config.home-manager.useGlobalPkgs = true;
   config.home-manager.useUserPackages = true;
 
-  config.home-manager.users."${specialArgs.var.${specialArgs.system}.username}" = import ./home-home.nix;# {  inherit specialArgs;  };
+  config.home-manager.users.${username} = import ./home-home.nix {  inherit specialArgs;  };
 }
