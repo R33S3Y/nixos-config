bea146fa1ecc212ce428bc2cc069c8d0ff19697b
R33S3Y <r3es3y@gmail.com>
2025-11-13 18:48:33 +1300

    2025-11-13 - Attempting Build for gen: 454

--- core/other/home-home.nix
+++ core/other/home-home.nix
@@ -5,9 +5,9 @@
   programs.home-manager.enable = true;
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  home.stateVersion = "24.11";
-  home.username = "reese";
-  home.homeDirectory = "/home/reese";
+  #home.stateVersion = "24.11";
+  #home.username = "reese";
+  #home.homeDirectory = "/home/reese";
 
 }
 
