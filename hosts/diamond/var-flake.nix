47f05123bb2af02f2f156e76251af38f6cf5be2e
R33S3Y <r3es3y@gmail.com>
2025-11-13 18:14:06 +1300

    2025-11-13 - Attempting Build for gen: 454

--- hosts/diamond/var-flake.nix
+++ hosts/diamond/var-flake.nix
@@ -9,7 +9,7 @@ let
     imports = [
       # Nix modules
       inputs.stylix.nixosModules.stylix
-      # home-manager.nixosModules.home-manager
+      inputs.home-manager.nixosModules.home-manager
       # CORE
       # boot
       ../../core/boot/uefi-nix.nix
