820e337074af7ce2217fc1aaa7244e660d81b7c0
R33S3Y <r3es3y@gmail.com>
2025-11-13 18:49:15 +1300

    2025-11-13 - Attempting Build for gen: 454

--- core/other/home-home.nix
+++ core/other/home-home.nix
@@ -1,13 +1,13 @@
-{ config, pkgs, specialArgs, ... }:
+{ specialArgs, ... }:
 {
   # imports = specialArgs.var.${specialArgs.system}.homeImports;
   # Enable home-manager
   programs.home-manager.enable = true;
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  #home.stateVersion = "24.11";
-  #home.username = "reese";
-  #home.homeDirectory = "/home/reese";
+  home.stateVersion = "24.11";
+  home.username = "reese";
+  home.homeDirectory = "/home/reese";
 
 }
 
