ce57551bdb270681ce8e5d514ec5431e774d0ebc
R33S3Y <r3es3y@gmail.com>
2025-11-30 10:27:03 +1300

    2025-11-30 - Attempting Build for gen: 491

--- core/other/home-nix.nix
+++ core/other/home-nix.nix
@@ -4,7 +4,10 @@
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
 
-  home-manager.extraSpecialArgs = specialArgs;
+  home-manager.extraSpecialArgs = {
+    inherit (specialArgs) var system; # keep your existing vars
+    nur = specialArgs.nur;            # pass nur through
+  };
 
   home-manager.users."${specialArgs.var.${specialArgs.system}.username}" = import ./home-home.nix;
 }
