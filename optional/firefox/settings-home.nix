fccb875c692b34376e54734ad78a22d82b4a3f8a
R33S3Y <r3es3y@gmail.com>
2025-11-30 14:34:20 +1300

    2025-11-30 - Attempting Build for gen: 493

--- optional/firefox/settings-home.nix
+++ optional/firefox/settings-home.nix
@@ -56,28 +56,7 @@
       # Valid strings for installation_mode are "allowed", "blocked",
       # "force_installed" and "normal_installed".
       ExtensionSettings = {
-        #"*".installation_mode = "blocked"; # blocks all addons except the ones specified below
-          # uBlock Origin:
-        "uBlock0@raymondhill.net" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-          installation_mode = "force_installed";
-        };
-        "addon@darkreader.org" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
-          installation_mode = "force_installed";
-        };
-        "contact@maxhu.dev" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/mtab/latest.xpi";
-          installation_mode = "force_installed";
-        };
-        "{34daeb50-c2d2-4f14-886a-7160b24d66a4}" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/file/4487339/youtube_shorts_block-1.5.3.xpi";
-          installation_mode = "force_installed";
-        };
-        "sponsorBlocker@ajay.app" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/file/4523436/sponsorblock-5.13.2.xpi";
-          installation_mode = "force_installed";
-        };
+        "*".installation_mode = "force_installed"; # blocks all addons except the ones specified below
       };
     };
   };
