e3ac5feca7a5d28d521c1032f2d6fcef9afec9a5
R33S3Y <r3es3y@gmail.com>
2025-12-10 21:27:22 +1300

    2025-12-10 - Attempting Build for gen: 517

--- optional/obsidian/enable-nix.nix
+++ optional/obsidian/enable-nix.nix
@@ -5,4 +5,12 @@
   environment.systemPackages = with pkgs; [
     obsidian
   ];
+
+
+  boot.supportedFilesystems = [ "nfs" ];
+  fileSystems."/home/reese/lapisLazuli" = {
+    device = "lapisLazuli:/mnt/Pool 1/lapisLazuli";
+    fsType = "nfs";
+  };
+
 }
