0f5267c3a3f8073d2346abda49351fbfc1370a13
R33S3Y <r3es3y@gmail.com>
2026-01-17 18:58:29 +1300

    2026-01-17 - Attempting Build for gen: 542

--- optional/other/alvr-nix.nix
+++ optional/other/alvr-nix.nix
@@ -2,7 +2,27 @@
 { config, pkgs, ... }:
 
 {
-  programs.alvr.enable = true;
+  programs.alvr.enable = {
+    # Pin to 20.13.0 due to https://github.com/alvr-org/ALVR/issues/3134
+    package = pkgs.alvr.overrideAttrs (
+      finalAttrs: prevAttrs: {
+        version = "20.13.0";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "alvr-org";
+          repo = "ALVR";
+          tag = "v${finalAttrs.version}";
+          fetchSubmodules = true;
+          hash = "sha256-h7/fuuolxbNkjUbqXZ7NTb1AEaDMFaGv/S05faO2HIc=";
+        };
+
+        cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+          inherit (finalAttrs) src;
+          hash = "sha256-A0ADPMhsREH1C/xpSxW4W2u4ziDrKRrQyY5kBDn//gQ=";
+        };
+      }
+    );
+  };
 
   networking.firewall.allowedUDPPorts = [ 9943 9944 ];
 }
