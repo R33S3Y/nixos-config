a72263ecc973cd8ec7fca751cc7f890c3cee06b8
R33S3Y <r3es3y@gmail.com>
2026-02-16 20:52:45 +1300

    2026-02-16 - Attempting Build for gen: 594

--- core/style/stylix-nix.nix
+++ core/style/stylix-nix.nix
@@ -1,49 +1,51 @@
 { config, pkgs, lib, ... }:
 {
-  stylix.enable = true;
-
-  stylix.polarity = "dark";
-
-  stylix.image = ./wallpaper.jpg;
-
-  stylix.base16Scheme = {
-    base00 = "181B28"; # ----
-    base01 = "282b38"; # ---
-    base02 = "383b48"; # --
-    base03 = "484b58"; # -
-    base04 = "c58bcf"; # +
-    base05 = "d5abdf"; # ++
-    base06 = "e5cbef"; # +++
-    base07 = "f5ebff"; # ++++
-    base08 = "4075dc"; # red
-    base09 = "5075dc"; # orange
-    base0A = "6075dc"; # yellow
-    base0B = "7075dc"; # green
-    base0C = "8075dc"; # aqua/cyan
-    base0D = "9075dc"; # blue
-    base0E = "a075dc"; # purple
-    base0F = "b075dc"; # brown
-  };
+  config.stylix = {
+    enable = true;
+    
+    polarity = "dark";
+    
+    image = ./wallpaper.jpg;
+    
+    base16Scheme = {
+      base00 = "181B28"; # ----
+      base01 = "282b38"; # ---
+      base02 = "383b48"; # --
+      base03 = "484b58"; # -
+      base04 = "c58bcf"; # +
+      base05 = "d5abdf"; # ++
+      base06 = "e5cbef"; # +++
+      base07 = "f5ebff"; # ++++
+      base08 = "4075dc"; # red
+      base09 = "5075dc"; # orange
+      base0A = "6075dc"; # yellow
+      base0B = "7075dc"; # green
+      base0C = "8075dc"; # aqua/cyan
+      base0D = "9075dc"; # blue
+      base0E = "a075dc"; # purple
+      base0F = "b075dc"; # brown
+    };
   
-  stylix.fonts = {
-    monospace = {
-      package = pkgs.nerd-fonts.jetbrains-mono;
-      name = "JetBrains Mono Nerd Font";
-    };
-    sansSerif = {
-      package = pkgs.source-sans-pro;
-      name = "Source Sans Pro";
-    };
-    serif = config.stylix.fonts.sansSerif;
-    emoji = {
-      package = pkgs.noto-fonts-color-emoji;
-      name = "Noto Color Emoji";
-    };
-    sizes = {
-      applications = 13;
-      desktop = 15;
-      popups = 13;
-      terminal = 11;
+    fonts = {
+      monospace = {
+        package = pkgs.nerd-fonts.jetbrains-mono;
+        name = "JetBrains Mono Nerd Font";
+      };
+      sansSerif = {
+        package = pkgs.source-sans-pro;
+        name = "Source Sans Pro";
+      };
+      serif = config.stylix.fonts.sansSerif;
+      emoji = {
+        package = pkgs.noto-fonts-color-emoji;
+        name = "Noto Color Emoji";
+      };
+      sizes = {
+        applications = 13;
+        desktop = 15;
+        popups = 13;
+        terminal = 11;
+      };
     };
   };
 
