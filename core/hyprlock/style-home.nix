86f26a2f2851c920672581668f05874a13e0c0f6
R33S3Y <r3es3y@gmail.com>
2026-02-16 20:06:52 +1300

    2026-02-16 - Attempting Build for gen: 578

--- core/hyprlock/style-home.nix
+++ core/hyprlock/style-home.nix
@@ -19,6 +19,7 @@
       };
 
       shape = [
+        # bottom window box
         {
           monitor = specialArgs.var.${specialArgs.system}.primaryMonitor;
           size = "400, 200";
@@ -26,7 +27,7 @@
           halign = "center";
           valign = "center";
 
-          color = "rgba(${config.stylix.base16Scheme.base00}E0)";
+          color = "rgba(${config.stylix.base16Scheme.base00}e0)";
           rounding = 10;
           border_size = 2;
           border_color = "rgba(${config.stylix.base16Scheme.base0D}ff)";
