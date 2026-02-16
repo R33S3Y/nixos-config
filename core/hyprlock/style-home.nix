14b476c00e9a678568ee2be2e203e53c1265b989
R33S3Y <r3es3y@gmail.com>
2026-02-16 18:57:22 +1300

    2026-02-16 - Attempting Build for gen: 562

--- core/hyprlock/style-home.nix
+++ core/hyprlock/style-home.nix
@@ -44,7 +44,7 @@
           monitor = "${specialArgs.var.${specialArgs.system}.primaryMonitor}";
           text = "$USER";
           color = "rgba(${config.stylix.base16Scheme.base04}ff)";
-          font_size = 28;
+          font_size = 25;
           position = "0, -180";
           halign = "center";
           valign = "center";
