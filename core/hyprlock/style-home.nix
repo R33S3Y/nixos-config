1819ec6123e67de5a072028581ff504c784672b7
R33S3Y <r3es3y@gmail.com>
2026-02-16 20:28:01 +1300

    2026-02-16 - Attempting Build for gen: 589

--- core/hyprlock/style-home.nix
+++ core/hyprlock/style-home.nix
@@ -21,7 +21,7 @@
         # window box
         {
           monitor = specialArgs.var.${specialArgs.system}.primaryMonitor;
-          size = "400, 200";
+          size = "500, 200";
           position = "0, -225";
           halign = "center";
           valign = "center";
@@ -40,7 +40,7 @@
           text = ''cmd[update:1000] echo "<span>$(date +"%I:%M")</span>"'';
           color = "rgba(${config.stylix.base16Scheme.base06}ff)";
           font_size = 100;
-          position = "0, 300";
+          position = "0, 150";
           halign = "center";
           valign = "center";
         }
@@ -50,7 +50,7 @@
           text = ''cmd[update:1000] echo -e "$(date +"%A, %B %d")"'';
           color = "rgba(${config.stylix.base16Scheme.base04}ff)";
           font_size = 30;
-          position = "0, 190";
+          position = "0, 100";
           halign = "center";
           valign = "center";
         }
@@ -60,7 +60,7 @@
           text = "$USER";
           color = "rgba(${config.stylix.base16Scheme.base04}ff)";
           font_size = 25;
-          position = "0, -180";
+          position = "0, -100";
           halign = "center";
           valign = "center";
         }
@@ -82,7 +82,7 @@
         fade_on_empty = false;
         placeholder_text = "<i>Enter Password</i>";
         hide_input = false;
-        position = "0, -250";
+        position = "0, -150";
         halign = "center";
         valign = "center";
       };
