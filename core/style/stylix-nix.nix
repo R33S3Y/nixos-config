653af8644ae74154c70e1e7909cd7e48f6ead388
R33S3Y <r3es3y@gmail.com>
2026-02-18 20:10:56 +1300

    2026-02-18 - Attempting Build for gen: 604

--- core/style/stylix-nix.nix
+++ core/style/stylix-nix.nix
@@ -5,26 +5,10 @@
     
     polarity = specialArgs.themes.${specialArgs.theme}.polarity;
     
-    image = specialArgs.themes.${specialArgs.theme}.wallpaper;
+    image = specialArgs.themes.${specialArgs.theme}.image;
     
     base16Scheme = specialArgs.themes.${specialArgs.theme}.base16Scheme;
   
     fonts = specialArgs.themes.${specialArgs.theme}.fonts;
   };
 }
-#base00 = "#181B28"; # ----
-#base01 = "#282b38"; # ---
-#base02 = "#383b48"; # --
-#base03 = "#484b58"; # -
-#base04 = "#c58bcf"; # +
-#base05 = "#d5abdf"; # ++
-#base06 = "#e5cbef"; # +++
-#base07 = "#f5ebff"; # ++++
-#base08 = "#4075dc"; # red
-#base09 = "#5075dc"; # orange
-#base0A = "#6075dc"; # yellow
-#base0B = "#7075dc"; # green
-#base0C = "#8075dc"; # aqua/cyan
-#base0D = "#9075dc"; # blue
-#base0E = "#a075dc"; # purple
-#base0F = "#b075dc"; # brown
