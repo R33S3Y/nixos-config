4a2496a3a9ac0c593401d3755f45e89303e80c28
R33S3Y <r3es3y@gmail.com>
2026-02-18 18:41:25 +1300

    2026-02-18 - Attempting Build for gen: 594

--- data/hosts/bort/var-flake.nix
+++ data/hosts/bort/var-flake.nix
@@ -11,43 +11,43 @@ let
       inputs.nix-minecraft.nixosModules.minecraft-servers
       # CORE
       # boot
-      ../../core/boot/bios-nix.nix
+      ../../../core/boot/bios-nix.nix
 
       # Kitty
-      ../../core/kitty/tools-nix.nix
+      ../../../core/kitty/tools-nix.nix
 
       # Networking
-      ../../core/networking/static-nix.nix
+      ../../../core/networking/static-nix.nix
 
       # Other
-      ../../core/other/local-nix.nix       # Local settings
-      ../../core/other/nix-nix.nix         # Nix settings  -  enable flakes, state nix version, etc
-      ../../core/other/programs-nix.nix    # Programs  -  Programs that are still needed. But dont need there own section
-      ../../core/other/user-nix.nix        # user  -  Adds a user
-      ../../core/other/ssh-nix.nix         # ssh  -  Enables ssh on port 22 needed for deploy rs
-      ../../core/other/sudo-nix.nix        # sudo  -  Sudo settings
-      ../../core/other/x11-nix.nix         # x11  -  needed for Xwayland??
-      ../../core/other/journald-nix.nix    # journald  -  adds a fix that explicity limits how much storage logs are allow to take at 2GB
+      ../../../core/other/local-nix.nix       # Local settings
+      ../../../core/other/nix-nix.nix         # Nix settings  -  enable flakes, state nix version, etc
+      ../../../core/other/programs-nix.nix    # Programs  -  Programs that are still needed. But dont need there own section
+      ../../../core/other/user-nix.nix        # user  -  Adds a user
+      ../../../core/other/ssh-nix.nix         # ssh  -  Enables ssh on port 22 needed for deploy rs
+      ../../../core/other/sudo-nix.nix        # sudo  -  Sudo settings
+      ../../../core/other/x11-nix.nix         # x11  -  needed for Xwayland??
+      ../../../core/other/journald-nix.nix    # journald  -  adds a fix that explicity limits how much storage logs are allow to take at 2GB
 
 
       # OPTIONAL
       # You can comment and uncomment these as needed
 
       # btop
-      ../../optional/btop/enable-nix.nix
+      ../../../optional/btop/enable-nix.nix
 
       # Fast Fetch
-      ../../optional/fastfetch/enable-nix.nix     # Fastfetch  -  You got to show something in that cmd for your reddit posts
+      ../../../optional/fastfetch/enable-nix.nix     # Fastfetch  -  You got to show something in that cmd for your reddit posts
 
       # LapisLazuli
-      ../../optional/lapisLazuli/nfs-nix.nix      # Lapius  -  NAS
+      ../../../optional/lapisLazuli/nfs-nix.nix      # Lapius  -  NAS
 
       # Services
-      ../../optional/services/minecraft/minecraft-nix.nix
-      ../../optional/services/minecraft/minecraftBackup-nix.nix
+      ../../../optional/services/minecraft/minecraft-nix.nix
+      ../../../optional/services/minecraft/minecraftBackup-nix.nix
      
       # Other
-      ../../optional/other/programs-nix.nix           # Install all programs in the programs var
+      ../../../optional/other/programs-nix.nix           # Install all programs in the programs var
 
       # My stuff
       ./hardware-configuration.nix   # hardware  -  your hardware settings
--- data/hosts/cinnabar/var-flake.nix
+++ data/hosts/cinnabar/var-flake.nix
@@ -13,67 +13,67 @@ let
       nur.modules.nixos.default
       # CORE
       # boot
-      ../../core/boot/uefi-nix.nix
-      ../../core/boot/style-nix.nix
+      ../../../core/boot/uefi-nix.nix
+      ../../../core/boot/style-nix.nix
 
       # Hyprland
-      ../../core/hyprland/enable-nix.nix
+      ../../../core/hyprland/enable-nix.nix
 
       # Hyprlock
-      ../../core/hyprlock/enable-nix.nix
+      ../../../core/hyprlock/enable-nix.nix
 
       # Hyprpanel
-      ../../core/hyprpanel/enable-nix.nix
+      ../../../core/hyprpanel/enable-nix.nix
 
       # Kitty
-      ../../core/kitty/enable-nix.nix
-      ../../core/kitty/tools-nix.nix
+      ../../../core/kitty/enable-nix.nix
+      ../../../core/kitty/tools-nix.nix
 
       # lazyUpdate - update on rebulid script - requres passwordless nixos-rebuild provided by sudo-nix.nix
-      ../../core/lazyUpdate/enable-nix.nix
+      ../../../core/lazyUpdate/enable-nix.nix
 
       # Networking
-      ../../core/networking/dhcp-nix.nix
+      ../../../core/networking/dhcp-nix.nix
   
       # PCmanFM
-      ../../core/pcmanfm/enable-nix.nix
+      ../../../core/pcmanfm/enable-nix.nix
 
       # Rofi
-      ../../core/rofi/enable-nix.nix
+      ../../../core/rofi/enable-nix.nix
   
       # SDDM
-      ../../core/sddm/enable-nix.nix
+      ../../../core/sddm/enable-nix.nix
 
       # Other
-      ../../core/other/home-nix.nix        # home
-      ../../core/other/journald-nix.nix    # journald  -  adds a fix that explicity limits how much storage logs are allow to take at 2GB
-      ../../core/other/local-nix.nix       # Local settings
-      ../../core/other/nix-nix.nix         # Nix settings  -  enable flakes, state nix version, etc
-      ../../core/other/programs-nix.nix    # Programs  -  Programs that are still needed. But dont need there own section
-      ../../core/other/user-nix.nix        # user  -  Adds a user
-      ../../core/other/ssh-nix.nix         # ssh  -  Enables ssh on port 22 needed for deploy rs
-      ../../core/style/stylix-nix.nix      # stylix  -  this repo expects stylix
-      ../../core/other/sudo-nix.nix        # sudo  -  Sudo settings
-      ../../core/other/x11-nix.nix         # x11  -  needed for Xwayland??
+      ../../../core/other/home-nix.nix        # home
+      ../../../core/other/journald-nix.nix    # journald  -  adds a fix that explicity limits how much storage logs are allow to take at 2GB
+      ../../../core/other/local-nix.nix       # Local settings
+      ../../../core/other/nix-nix.nix         # Nix settings  -  enable flakes, state nix version, etc
+      ../../../core/other/programs-nix.nix    # Programs  -  Programs that are still needed. But dont need there own section
+      ../../../core/other/user-nix.nix        # user  -  Adds a user
+      ../../../core/other/ssh-nix.nix         # ssh  -  Enables ssh on port 22 needed for deploy rs
+      ../../../core/style/stylix-nix.nix      # stylix  -  this repo expects stylix
+      ../../../core/other/sudo-nix.nix        # sudo  -  Sudo settings
+      ../../../core/other/x11-nix.nix         # x11  -  needed for Xwayland??
 
 
       # OPTIONAL
       # You can comment and uncomment these as needed
 
       # btop
-      ../../optional/btop/enable-nix.nix
+      ../../../optional/btop/enable-nix.nix
 
       # Fast Fetch
-      ../../optional/fastfetch/enable-nix.nix     # Fastfetch  -  You got to show something in that cmd for your reddit posts
+      ../../../optional/fastfetch/enable-nix.nix     # Fastfetch  -  You got to show something in that cmd for your reddit posts
 
       # Firefox
-      ../../optional/firefox/enable-nix.nix
+      ../../../optional/firefox/enable-nix.nix
 
       # LapisLazuli
-      ../../optional/lapisLazuli/nfs-nix.nix      # Lapius  -  NAS
+      ../../../optional/lapisLazuli/nfs-nix.nix      # Lapius  -  NAS
 
       # other
-      ../../optional/other/programs-nix.nix
+      ../../../optional/other/programs-nix.nix
 
       # My stuff
       ./hardware-configuration.nix  # hardware  -  your hardware settings
@@ -82,48 +82,48 @@ let
     homeImports = [
       # CORE
       # Hyprland
-      ../../core/hyprland/bind-home.nix    # Keyboard bindings
-      ../../core/hyprland/settings-home.nix# Settings
-      ../../core/hyprland/style-home.nix   # Styles tweaks  -  (Most styling is handled by stylix)
+      ../../../core/hyprland/bind-home.nix    # Keyboard bindings
+      ../../../core/hyprland/settings-home.nix# Settings
+      ../../../core/hyprland/style-home.nix   # Styles tweaks  -  (Most styling is handled by stylix)
 
       # Hyprlock
-      ../../core/hyprlock/style-home.nix   # Styles + What to display and where
+      ../../../core/hyprlock/style-home.nix   # Styles + What to display and where
 
       # Hyprpanel
-      ../../core/hyprpanel/style-home.nix
+      ../../../core/hyprpanel/style-home.nix
 
       # Kitty
-      ../../core/kitty/bind-home.nix       # Key binds
-      ../../core/kitty/style-home.nix      # Styles  -  You should be fine to get away with disabling this
-      ../../core/kitty/settings-home.nix   # Settings
+      ../../../core/kitty/bind-home.nix       # Key binds
+      ../../../core/kitty/style-home.nix      # Styles  -  You should be fine to get away with disabling this
+      ../../../core/kitty/settings-home.nix   # Settings
 
       # lazyUpdate - update on rebulid script - requres passwordless nixos-rebuild provided by sudo-nix.nix
-      ../../core/lazyUpdate/run-home.nix
+      ../../../core/lazyUpdate/run-home.nix
 
       # PCmanFM
       # No home-manager files
 
       # Rofi
-      ../../core/rofi/style-home.nix       # Styles
+      ../../../core/rofi/style-home.nix       # Styles
 
       # SDDM
       # TODO : SDDM styles
 
       # Other
-      ../../core/other/xdgMime-home.nix   # Sets default apps
+      ../../../core/other/xdgMime-home.nix   # Sets default apps
 
 
       # OPTIONAL
       # You can comment and uncomment these as needed
 
       # btop
-      ../../optional/btop/style-home.nix
+      ../../../optional/btop/style-home.nix
 
       # Fast Fetch
-      ../../optional/fastfetch/settings-home.nix  # Fastfetch  -  run on bash init
+      ../../../optional/fastfetch/settings-home.nix  # Fastfetch  -  run on bash init
 
       # Firefox
-      ../../optional/firefox/settings-home.nix
+      ../../../optional/firefox/settings-home.nix
 
       # Other
       # No home-manager files
