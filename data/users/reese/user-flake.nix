b0c19231445ec1081d036202658d1c6206ee38e0
R33S3Y <r3es3y@gmail.com>
2026-02-18 19:39:10 +1300

    2026-02-18 - Attempting Build for gen: 595

---
+++ data/users/reese/user-flake.nix
@@ -0,0 +1,31 @@
+{ inputs }:
+
+let
+  reese = {
+    name = "reese";
+    prettyName = "Reese";
+
+    profile = ./profile.png;
+    theme = "diamond";
+
+    screenshotFolder = "~/Pictures";
+
+
+    timeZone = "Pacific/Auckland";
+
+    defaultLocale = "en_NZ.UTF-8";
+
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_NZ.UTF-8";
+      LC_IDENTIFICATION = "en_NZ.UTF-8";
+      LC_MEASUREMENT = "en_NZ.UTF-8";
+      LC_MONETARY = "en_NZ.UTF-8";
+      LC_NAME = "en_NZ.UTF-8";
+      LC_NUMERIC = "en_NZ.UTF-8";
+      LC_PAPER = "en_NZ.UTF-8";
+      LC_TELEPHONE = "en_NZ.UTF-8";
+      LC_TIME = "en_NZ.UTF-8";
+    };
+  };
+in
+reese
