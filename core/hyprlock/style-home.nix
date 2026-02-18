a49eb00bc2354feb2ab12f7da9f2d3182fab4d4d
R33S3Y <r3es3y@gmail.com>
2026-02-18 19:59:52 +1300

    2026-02-18 - Attempting Build for gen: 602

--- core/hyprlock/style-home.nix
+++ core/hyprlock/style-home.nix
@@ -38,7 +38,7 @@
         {
           monitor = specialArgs.hosts.${specialArgs.host}.primaryMonitor;
           size = "500, 700";
-          position = "0, -50";
+          position = "0, -100";
           halign = "center";
           valign = "center";
 
