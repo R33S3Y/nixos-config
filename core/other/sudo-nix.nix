f29e5d18782c7ea26f3feed280ada08d37bc514e
R33S3Y <r3es3y@gmail.com>
2026-03-08 11:33:40 +1300

    2026-03-08 - Attempting Build for gen: 674

--- core/other/sudo-nix.nix
+++ core/other/sudo-nix.nix
@@ -13,10 +13,10 @@
     commands = [{
       command = "/run/current-system/sw/bin/nixos-rebuild";
       options = [ "NOPASSWD" ];
-    }, {
+    } {
       command = "/run/current-system/sw/bin/nix-env --profile /nix/var/nix/profiles/system --delete-generations +25";
       options = [ "NOPASSWD" ];
-    }, {
+    } {
       command = "/run/current-system/sw/bin/nix-collect-garbage";
       options = [ "NOPASSWD" ];
     }];
