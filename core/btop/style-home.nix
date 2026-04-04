1210af6991da30c10f46002b04ae6e726a49a947
R33S3Y <r3es3y@gmail.com>
2026-04-05 07:41:34 +1200

    2026-04-05 - Attempting Build for gen: 684

--- core/btop/style-home.nix
+++ core/btop/style-home.nix
@@ -5,7 +5,7 @@
     settings = {
       color_theme = "stylix";
       update_ms = 1000;
-      shown_boxes = "cpu mem net proc gpu0";
+      shown_boxes = "cpu mem net proc gpu1 gpu2";
       presets = "cpu:1:default,proc:0:default cpu:0:default,mem:0:default,net:0:default";
     };
   };
