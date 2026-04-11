f1881fb185fd9d552991b64b8fa1a655920ab53a
R33S3Y <r3es3y@gmail.com>
2026-04-12 11:27:42 +1200

    2026-04-12 - Attempting Build for gen: 692

--- desktop/obsidian/settings-home.nix
+++ desktop/obsidian/settings-home.nix
@@ -8,11 +8,13 @@
     enable = true;
     defaultSettings.app = {
       "useMarkdownLinks" = true;
+      "newLinkFormat" = "relative";
     };
     vaults = {
       "lapisLazuli/reese/NoteBox".settings.app = {
         "attachmentFolderPath" = "Assets";
         "useMarkdownLinks" = true;
+        "newLinkFormat" = "relative";
       };
     };
   };
