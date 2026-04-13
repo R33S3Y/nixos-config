20e3812bf28a31683282e55ec9e343260758c6bc
R33S3Y <r3es3y@gmail.com>
2026-04-13 22:45:14 +1200

    2026-04-13 - Attempting Build for gen: 696

--- desktop/vscode/settings-home.nix
+++ desktop/vscode/settings-home.nix
@@ -16,16 +16,6 @@
         pkgs.vscode-extensions.naumovs.color-highlight 
         pkgs.vscode-extensions.ms-python.python
       ];
-      userSettings = {
-        "chat.editor.fontSize" = 14;
-        "debug.console.fontSize" = 14;
-        "editor.fontSize" = 14;
-        "editor.minimap.sectionHeaderFontSize" = 9;
-        "markdown.preview.fontSize" = 14;
-        "scm.inputFontSize" = 13;
-        "screencastMode.fontSize" = 58;
-        "terminal.integrated.fontSize" = 14;
-      }
     };
   };
 }
