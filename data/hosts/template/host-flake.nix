b3467a7d4782239ed595aedf6ed02e2a6728b25d
R33S3Y <r3es3y@gmail.com>
2026-04-14 22:17:55 +1200

    2026-04-14 - Attempting Build for gen: 708

--- data/hosts/template/host-flake.nix
+++ data/hosts/template/host-flake.nix
@@ -137,7 +137,8 @@ let
       ../../../desktop/strawberry/bind-home.nix     # Global Hotkeys for music player
       # VScode
       ../../../desktop/vscode/settings-home.nix
-      ../../../desktop/vscode/language/nix-home.nix # Nix LSP and FMT support
+      ../../../desktop/vscode/language/markdown-home.nix   # Markdown
+      ../../../desktop/vscode/language/nix-home.nix        # Nix LSP and FMT support
 
     ];
     
---
+++ desktop/vscode/language/markdown-home.nix
@@ -0,0 +1,11 @@
+{ ... }:
+
+{ 
+  programs.vscode = {
+    profiles.default = {
+      userSettings.markdown = {
+        typographer = true;
+      };
+    };
+  };
+}
