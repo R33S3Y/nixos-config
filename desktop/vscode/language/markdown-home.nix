aa089a26382085d98772f7398d85699bf1f6532e
R33S3Y <r3es3y@gmail.com>
2026-04-14 22:50:06 +1200

    2026-04-14 - Attempting Build for gen: 708

--- desktop/vscode/language/markdown-home.nix
+++ desktop/vscode/language/markdown-home.nix
@@ -1,8 +1,15 @@
-{ ... }:
+{ pkgs, ... }:
 
 { 
+  home.packages = with pkgs; [ 
+    markdown-oxide 
+  ];
+
   programs.vscode = {
     profiles.default = {
+      extensions = with pkgs; [ 
+        markdown-oxide
+      ];
       userSettings = {
         markdown.typographer = true;
         workbench.editorAssociations = {
