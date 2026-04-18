9f0c4fdd01db3132f6681867a34ad17ceaaabcfa
R33S3Y <r3es3y@gmail.com>
2026-04-18 23:32:51 +1200

    2026-04-18 - Attempting Build for gen: 711

--- desktop/vscode/settings-home.nix
+++ desktop/vscode/settings-home.nix
@@ -17,8 +17,18 @@
       userSettings = { 
         editor = {
           tabSize = 2; # sets tabsize
+          insertSpaces = true; # Space when tab
           detectIndentation = false; # turns off auto detect for tabSize
         };
+        files = { 
+          trimTrailingWhitespace = true;
+          insertFinalNewline = true;
+          trimFinalNewlines = true;
+        };
+        editor = {
+          formatOnSave = true;
+          formatOnPaste = true;
+        };         
       };
     };
   };
