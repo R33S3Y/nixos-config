ad5ab0ee51b6fea3d856a9b873d4a886d3759eb1
R33S3Y <r3es3y@gmail.com>
2026-04-19 07:54:12 +1200

    2026-04-19 - Attempting Build for gen: 715

--- desktop/vscode/language/javascript-home.nix
+++ desktop/vscode/language/javascript-home.nix
@@ -5,6 +5,7 @@
     profiles.default = {
       extensions = with pkgs; [
         vscode-extensions.dbaeumer.vscode-eslint
+        vscode-extensions.prettier.prettier-vscode
       ];
       userSettings = {
 
