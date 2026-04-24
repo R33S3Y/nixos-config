a377c2ad1387eb1b1421ce52d477891cdbb6ce7e
R33S3Y <r3es3y@gmail.com>
2026-04-25 11:07:43 +1200

    2026-04-25 - Attempting Build for gen: 729

--- core/other/home-home.nix
+++ core/other/home-home.nix
@@ -1,15 +1,15 @@
 { config, ... }:
 {
 
-  #imports = config.system.hosts.${config.system.host}.homeImports;
+  imports = config.system.hosts.${config.system.host}.homeImports;
   # Enable home-manager
   config.programs.home-manager.enable = true;
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  #config.home = {
-  #  stateVersion = "24.11";
-  #  username = config.system.users.${config.system.user}.name;
-  #  homeDirectory = "/home/${config.system.users.${config.system.user}.name}";
-  #};
+  home = {
+    stateVersion = "24.11";
+    username = config.system.users.${config.system.user}.name;
+    homeDirectory = "/home/${config.system.users.${config.system.user}.name}";
+  };
 
 }
