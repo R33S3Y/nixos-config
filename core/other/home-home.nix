a232d1bfcbff29bb7a86e1e9e531cf1f03c430d9
R33S3Y <r3es3y@gmail.com>
2026-04-24 22:01:01 +1200

    2026-04-24 - Attempting Build for gen: 728

--- core/other/home-home.nix
+++ core/other/home-home.nix
@@ -37,10 +37,10 @@
 
   imports = specialArgs.hosts.${specialArgs.host}.homeImports;
   # Enable home-manager
-  #programs.home-manager.enable = true;
+  programs.home-manager.enable = true;
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  #home.stateVersion = "24.11";
-  #home.username = specialArgs.users.${specialArgs.user}.name;
-  #home.homeDirectory = "/home/${specialArgs.users.${specialArgs.user}.name}";
+  home.stateVersion = "24.11";
+  home.username = specialArgs.users.${specialArgs.user}.name;
+  home.homeDirectory = "/home/${specialArgs.users.${specialArgs.user}.name}";
 }
