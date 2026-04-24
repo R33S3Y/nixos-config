930117560142b83c0b3384f75ff28f646c5ff065
R33S3Y <r3es3y@gmail.com>
2026-04-24 23:30:57 +1200

    2026-04-24 - Attempting Build for gen: 728

--- core/other/system-nix.nix
+++ core/other/system-nix.nix
@@ -1,37 +1,49 @@
-{ lib, specialArgs, ... }:
+{ lib, config, ... }:
+
+let
+  args = config._module.args;
+in
 {
   options.system = {
     hosts = lib.mkOption {
-      default = specialArgs.hosts;
       type = lib.types.attrs;
       readOnly = true;
     };
+
     host = lib.mkOption {
-      default = specialArgs.host;
       type = lib.types.str;
       readOnly = true;
     };
 
     users = lib.mkOption {
-      default = specialArgs.users;
       type = lib.types.attrs;
       readOnly = true;
     };
+
     user = lib.mkOption {
-      default = specialArgs.user;
       type = lib.types.str;
       readOnly = true;
     };
 
     themes = lib.mkOption {
-      default = specialArgs.themes;
       type = lib.types.attrs;
       readOnly = true;
     };
+
     theme = lib.mkOption {
-      default = specialArgs.theme;
       type = lib.types.str;
       readOnly = true;
     };
+  };
+
+  config = {
+    system.hosts = args.hosts;
+    system.host  = args.host;
+
+    system.users = args.users;
+    system.user  = args.user;
+
+    system.themes = args.themes;
+    system.theme  = args.theme;
   };
 }
