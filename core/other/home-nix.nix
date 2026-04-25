7b14b4f7cfd3250c5c0fed0adc8d2e6ccb3ce210
R33S3Y <r3es3y@gmail.com>
2026-04-25 15:55:50 +1200

    2026-04-25 - Attempting Build for gen: 730

--- core/other/home-nix.nix
+++ core/other/home-nix.nix
@@ -4,7 +4,7 @@
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
 
-  home-manager.extraSpecialArgs = specialArgs;
+  home-manager.extraSpecialArgs = { system = config.system; };
 
   home-manager.users."${config.system.user}" = import ./system-home.nix;
 }
