f605310f52de305a20f1d03b039d023f52675d55
R33S3Y <r3es3y@gmail.com>
2026-04-26 15:44:31 +1200

    2026-04-26 - Attempting Build for gen: 735

--- core/networking/static-nix.nix
+++ core/networking/static-nix.nix
@@ -1,27 +1,23 @@
 { system, ... }:
 let
-  static =
-    { system }:
-    {
-      useDHCP = false;
-      interfaces.${system.hosts.${system.host}.static.interface}.ipv4.addresses = [
-        {
-          address = system.hosts.${system.host}.static.ipv4.address;
-          prefixLength = system.hosts.${system.host}.static.ipv4.prefixLength;
-        }
-      ];
-      defaultGateway = {
-        address = system.hosts.${system.host}.static.gatewayAddress;
-        interface = system.hosts.${system.host}.static.interface;
-      };
-      nameservers = system.hosts.${system.host}.static.nameservers;
-      networkmanager.enable = false;
+  static = system: {
+    useDHCP = false;
+    interfaces.${system.hosts.${system.host}.static.interface}.ipv4.addresses = [
+      {
+        address = system.hosts.${system.host}.static.ipv4.address;
+        prefixLength = system.hosts.${system.host}.static.ipv4.prefixLength;
+      }
+    ];
+    defaultGateway = {
+      address = system.hosts.${system.host}.static.gatewayAddress;
+      interface = system.hosts.${system.host}.static.interface;
     };
-  dhcp =
-    { system }:
-    {
-      networkmanager.enable = true;
-    };
+    nameservers = system.hosts.${system.host}.static.nameservers;
+    networkmanager.enable = false;
+  };
+  dhcp = system: {
+    networkmanager.enable = true;
+  };
 in
 {
   # Networking
