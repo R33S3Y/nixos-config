a75e8bd2f3825d23b86cdd604315cbd3d25ecbe0
R33S3Y <r3es3y@gmail.com>
2026-05-01 16:16:10 +1200

    2026-05-01 - Attempting Build for gen: 736

--- services/authDNS/knot-nix.nix
+++ services/authDNS/knot-nix.nix
@@ -20,7 +20,7 @@
 
       zone = lib.mapAttrs (name: info: {
         domain = name;
-        storage = "${pkgs."${name}.zone"}";
+        storage = "${config.environment.systemPackages."${name}.zone"}";
         file = "${name}.zone";
       }) config.services.authDNS.domains;
 
