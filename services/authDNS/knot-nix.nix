ee44875205cbd8b7664e405439fadddc65782d34
R33S3Y <r3es3y@gmail.com>
2026-05-01 16:49:21 +1200

    2026-05-01 - Attempting Build for gen: 736

--- services/authDNS/knot-nix.nix
+++ services/authDNS/knot-nix.nix
@@ -4,6 +4,47 @@
   pkgs,
   ...
 }:
+let
+  zones =
+    domains:
+    lib.mapAttrsToList (
+      name: info:
+      pkgs.stdenv.mkDerivation {
+        name = "${name}.zone";
+        pname = "${name}.zone";
+        version = "1.0";
+
+        src = null;
+        dontUnpack = true;
+
+        buildPhase = ''
+          echo "
+          $ORIGIN ${name}.
+          $TTL ${toString info.ttl}
+
+          @ IN SOA ns1.${name}. ${lib.strings.replaceString "@" "." info.email}. (
+            $(date +%s)                 ; serial (version number)
+            ${toString info.ttl}      ; refresh (how often should the secondary server contact main)
+            ${toString (info.ttl / 2)}  ; retry (how long should the secondary server wait before try to contact the primary server in the case of failure)
+            ${toString (info.ttl * 16)} ; expire (If the secondary fails to connect to the primary for this amount of time. It should stop serving requests.)
+            ${toString info.ttl}      ; minimum
+          )
+
+          @   IN NS ns1.${name}.
+
+          ${lib.concatStrings (
+            lib.map (record: "${record.name} IN ${record.type} ${record.value} \n") info.records
+          )}
+          " >> ${name}.zone
+        '';
+
+        installPhase = ''
+          mkdir -p $out
+          cp ${name}.zone $out
+        '';
+      }
+    ) config.services.authDNS.domains;
+in
 {
   services.knot = {
     enable = true;
@@ -18,11 +59,11 @@
         ];
       };
 
-      zone = lib.mapAttrs (name: info: {
+      zone = lib.mapAttrs (name: drv: {
         domain = name;
-        storage = "${pkgs."${name}.zone"}";
+        storage = drv;
         file = "${name}.zone";
-      }) config.services.authDNS.domains;
+      }) zones;
 
       log = {
         target = "syslog";
