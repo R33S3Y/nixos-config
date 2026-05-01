f060330cef09d3857ea7ddb2ecf983564a70b45f
R33S3Y <r3es3y@gmail.com>
2026-05-01 12:33:46 +1200

    2026-05-01 - Attempting Build for gen: 736

--- services/authDNS/zoneFiles-nix.nix
+++ services/authDNS/zoneFiles-nix.nix
@@ -8,7 +8,7 @@
 # this file takes all the info in services.authDNS.domains and converts it into a zone files and then pkgs it. For knot DNS to pickup and use.
 
 {
-  environment.systemPackages = lib.mapAttrsToList (
+  environment.systemPackages = lib.mapAttrs (
     name: info:
     pkgs.stdenv.mkDerivation {
       pname = "${name}.zone";
@@ -20,11 +20,11 @@
         $TTL ${info.ttl}
 
         @ IN SOA ns1.${name}. ${lib.strings.replaceString "@" "." info.email}. (
-          $(date +%s)      ; serial (version number)
-          ${info.ttl}      ; refresh (how often should the secondary server contact main)
-          ${info.ttl / 2}  ; retry (how long should the secondary server wait before try to contact the primary server in the case of failure)
-          ${info.ttl * 16} ; expire (If the secondary fails to connect to the primary for this amount of time. It should stop serving requests.)
-          ${info.ttl}      ; minimum
+          $(date +%s)                 ; serial (version number)
+          ${toString (info.ttl)}      ; refresh (how often should the secondary server contact main)
+          ${toString (info.ttl / 2)}  ; retry (how long should the secondary server wait before try to contact the primary server in the case of failure)
+          ${toString (info.ttl * 16)} ; expire (If the secondary fails to connect to the primary for this amount of time. It should stop serving requests.)
+          ${toString (info.ttl)}      ; minimum
         )
 
         @   IN NS ns1.${name}.
