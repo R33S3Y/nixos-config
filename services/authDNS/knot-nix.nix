60a95bc04ec728cff900fceacd800df72bc124c6
R33S3Y <r3es3y@gmail.com>
2026-05-08 07:24:47 +1200

    2026-05-08 - Attempting Build for gen: 736

--- services/authDNS/knot-nix.nix
+++ services/authDNS/knot-nix.nix
@@ -4,58 +4,30 @@
   pkgs,
   ...
 }:
-let
-  zones = lib.mapAttrs (
-    name: info:
-    pkgs.stdenv.mkDerivation {
-      pname = "${name}.zone";
-      version = "1.0";
-      src = null;
-      dontUnpack = true;
-      buildPhase = ''
-        cat > ${name}.zone <<EOF
-        \$ORIGIN ${name}.
-        \$TTL ${toString info.ttl}
-        @ IN SOA ns1.${name}. ${lib.strings.replaceStrings [ "@" ] [ "." ] info.email}. (
-          $(date +%s)                 ; serial
-          ${toString info.ttl}        ; refresh
-          ${toString (info.ttl / 2)}  ; retry
-          ${toString (info.ttl * 16)} ; expire
-          ${toString info.ttl}        ; minimum
-        )
-        @ IN NS ns1.${name}.
-        ${lib.concatMapStrings (record: "${record.name} IN ${record.type} ${record.value}\n") info.records}
-        EOF
-      '';
-      installPhase = ''
-        mkdir -p $out
-        cp ${name}.zone $out/
-      '';
-    }
-  ) config.services.authDNS.domains;
-in
 {
   services.knot = {
     enable = true;
     checkConfig = true;
+
     settings = {
+
       server = {
         listen = [
           "0.0.0.0@53"
           "::@53"
         ];
       };
-      zone = lib.mapAttrsToList (name: drv: {
+
+      zone = lib.mapAttrs (name: info: {
         domain = name;
-        storage = "${drv}";
+        storage = "${pkgs."${name}.zone"}";
         file = "${name}.zone";
-      }) zones;
-      log = [
-        {
-          target = "syslog";
-          any = "info";
-        }
-      ];
+      }) config.services.authDNS.domains;
+
+      log = {
+        target = "syslog";
+        any = "info";
+      };
     };
   };
 }
--- services/authDNS/zoneFiles-nix.nix
+++ services/authDNS/zoneFiles-nix.nix
@@ -8,5 +8,42 @@
 # this file takes all the info in services.authDNS.domains and converts it into a zone files and then pkgs it. For knot DNS to pickup and use.
 
 {
+  environment.systemPackages = lib.mapAttrsToList (
+    name: info:
+    pkgs.stdenv.mkDerivation {
+      name = "${name}.zone";
+      pname = "${name}.zone";
+      version = "1.0";
+
+      src = null;
+      dontUnpack = true;
+
+      buildPhase = ''
+        echo "
+        $ORIGIN ${name}.
+        $TTL ${toString info.ttl}
+
+        @ IN SOA ns1.${name}. ${lib.strings.replaceString "@" "." info.email}. (
+          $(date +%s)                 ; serial (version number)
+          ${toString info.ttl}      ; refresh (how often should the secondary server contact main)
+          ${toString (info.ttl / 2)}  ; retry (how long should the secondary server wait before try to contact the primary server in the case of failure)
+          ${toString (info.ttl * 16)} ; expire (If the secondary fails to connect to the primary for this amount of time. It should stop serving requests.)
+          ${toString info.ttl}      ; minimum
+        )
+
+        @   IN NS ns1.${name}.
+
+        ${lib.concatStrings (
+          lib.map (record: "${record.name} IN ${record.type} ${record.value} \n") info.records
+        )}
+        " >> ${name}.zone
+      '';
+
+      installPhase = ''
+        mkdir -p $out
+        cp ${name}.zone $out
+      '';
+    }
+  ) config.services.authDNS.domains;
 
 }
