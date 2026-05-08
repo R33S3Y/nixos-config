190bbfbed50731df9dc0f3407cb8c2ea612b3667
R33S3Y <r3es3y@gmail.com>
2026-05-09 10:54:12 +1200

    2026-05-09 - Attempting Build for gen: 739

--- data/networks/homelab/network-flake.nix
+++ data/networks/homelab/network-flake.nix
@@ -1,8 +1,10 @@
 { ... }:
-
-{
-  public = {
-    ipV4Address = "103.224.131.41";
-    domain = "reesey.org";
+let
+  network = {
+    public = {
+      ipV4Address = "103.224.131.41";
+      domain = "reesey.org";
+    };
   };
-}
+in
+network
