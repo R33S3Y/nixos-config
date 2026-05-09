415a7019deade52543296e4a6c91534ed230ee33
R33S3Y <r3es3y@gmail.com>
2026-05-09 19:56:25 +1200

    2026-05-09 - Attempting Build for gen: 740

--- services/authDNS/settings-nix.nix
+++ services/authDNS/settings-nix.nix
@@ -1,7 +1,16 @@
 { system, ... }:
 {
-  services.authDNS.domains.${system.networks.${system.network}.public.domain}.primaryNameserver = {
-    name = system.hosts.${system.host}.hostName;
-    address = system.networks.${system.network}.public.ipV4Address;
+  services.authDNS.domains.${system.networks.${system.network}.public.domain} = {
+    primaryNameserver = {
+      name = system.hosts.${system.host}.hostName;
+      address = system.networks.${system.network}.public.ipV4Address;
+    };
+    records = [
+      {
+        name = "${system.hosts.${system.host}.hostName}2";
+        type = "A";
+        value = system.networks.${system.network}.public.ipV4Address;
+      }
+    ];
   };
 }
