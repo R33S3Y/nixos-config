941a983512166da9fd2a2bb179146b20f0f495ff
R33S3Y <r3es3y@gmail.com>
2026-05-14 17:37:07 +1200

    2026-05-14 - Attempting Build for gen: 790

--- core/deploy/src/resolve.cpp
+++ core/deploy/src/resolve.cpp
@@ -52,6 +52,9 @@ void resolve::preprocessFile(const string &filepath) {
   }
 
   this->fileStr = fileStr;
+  if (filepath.find("flake.nix") != string::npos) {
+    cout << fileStr + "\n";
+  }
   return;
 }
 
