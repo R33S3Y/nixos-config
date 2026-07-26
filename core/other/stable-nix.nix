a67f4758e55912fd9414d2ff37278a3074737402
Reese <r3es3y@gmail.com>
2026-07-26 19:37:06 +1200

    2026-07-26 - Attempting Build for gen: 1248

--- core/other/stable-nix.nix
+++ core/other/stable-nix.nix
@@ -2,7 +2,7 @@
 {
   nixpkgs.overlays = [
     (final: prev: {
-      stable = inputs.nixpkgsStable {
+      stable = import inputs.nixpkgsStable {
         system = "x86_64-linux";
         config.allowUnfree = true;
       };
