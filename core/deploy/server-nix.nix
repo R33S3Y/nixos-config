c28871eefa494c0fb1a2d89e274d952d9c4ec0bd
Reese <r3es3y@gmail.com>
2026-08-02 21:24:55 +1200

    2026-08-02 - Attempting Build for gen: 6

--- core/deploy/server-nix.nix
+++ core/deploy/server-nix.nix
@@ -35,8 +35,11 @@ in
               -g \
               -I${prev.nlohmann_json}/include
 
+              -I${prev.libtar}/include \
+              -L${prev.libtar}/lib -ltar \
 
-
+              -I${prev.libssh2.dev}/include \
+              -L${prev.libssh2}/lib -lssh2 \
 
             sed -i 's/version/\"${version}\"/' server/man.md
             pandoc server/man.md -s -t man -o deploy.1
