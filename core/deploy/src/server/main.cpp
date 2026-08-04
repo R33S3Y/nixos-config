123a10e16b779f29adaf140f2b63f9d1d5c0e583
Reese <r3es3y@gmail.com>
2026-08-04 18:37:25 +1200

    2026-08-04 - Attempting Build for gen: 18

--- core/deploy/src/server/main.cpp
+++ core/deploy/src/server/main.cpp
@@ -151,12 +151,8 @@ int main(int argc, char const *argv[]) {
   }
 
   // append nixos config
-  size_t realnameLen = flakePath.size();
-  char *realname = new char[realnameLen + 1];
-  strncpy(realname, flakePath.c_str(), flakePath.size() + 1);
-
-  char savename[] = "nixosConfig";
-  if (tar_append_tree(tarball, realname, savename) != 0) {
+  if (tar_append_tree(tarball, const_cast<char *>("nixosConfig"),
+                      const_cast<char *>(flakePath.c_str())) != 0) {
     cerr << ttyHelper::error("tar_append_tree failed");
     filesystem::remove_all(tmpPath);
     return 1;
