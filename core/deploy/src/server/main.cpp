b7e16f17137ca6df503768cebf0421b194fffff0
Reese <r3es3y@gmail.com>
2026-08-11 21:20:03 +1200

    2026-08-11 - Attempting Build for gen: 29

--- core/deploy/src/server/main.cpp
+++ core/deploy/src/server/main.cpp
@@ -155,7 +155,7 @@ int main(int argc, char const *argv[]) {
     filesystem::remove_all(tmpPath);
     return 1;
   }
-
+  return 0;
   // make hash
   const sslHelper::result<vector<unsigned char>> sslHashStatus =
       sslHelper::getSHA256Hash(*tarballFileResult.output);
