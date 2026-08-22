94668843a6679cec5a30296227d49c8e96374ada
Reese <r3es3y@gmail.com>
2026-08-23 09:25:55 +1200

    2026-08-23 - Attempting Build for gen: 78

--- desktop/kitty/style-home.nix
+++ desktop/kitty/style-home.nix
@@ -13,7 +13,7 @@
       color0 = "#${config.stylix.base16Scheme.base00}"; # Black
       color1 = "#${config.stylix.base16Scheme.base08}"; # RED
       color2 = "#${config.stylix.base16Scheme.base0B}"; # GREEN
-      color3 = "#${config.stylix.base16Scheme.base0A}"; # YELLOW (Highlight?)
+      color3 = "#${config.stylix.base16Scheme.base09}"; # YELLOW (Highlight?)
       color4 = "#${config.stylix.base16Scheme.base0D}"; # BLUE
       color5 = "#${config.stylix.base16Scheme.base03}"; # MAGENTA
       color6 = "#${config.stylix.base16Scheme.base05}"; # CYAN
@@ -21,7 +21,7 @@
       color8 = "#${config.stylix.base16Scheme.base01}"; # BR BLACK
       color9 = "#${config.stylix.base16Scheme.base0F}"; # BR RED
       color10 = "#${config.stylix.base16Scheme.base0C}"; # BR GREEN
-      color11 = "#${config.stylix.base16Scheme.base09}"; # BR YELLOW
+      color11 = "#${config.stylix.base16Scheme.base0A}"; # BR YELLOW
       color12 = "#${config.stylix.base16Scheme.base0E}"; # BR BLUE
       color13 = "#${config.stylix.base16Scheme.base02}"; # BR MAGENTA
       color14 = "#${config.stylix.base16Scheme.base04}"; # BR CYAN
