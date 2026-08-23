66a51399157c7dcf3573fd7c44eed456fc3c4fab
Reese <r3es3y@gmail.com>
2026-08-24 04:05:39 +1200

    2026-08-24 - Attempting Build for gen: 84

--- core/deploy/src/server/main.cpp
+++ core/deploy/src/server/main.cpp
@@ -199,7 +199,7 @@ int main(int argc, char const *argv[]) {
     filesystem::remove_all(tmpPath);
     return 1;
   }
-  cout << "got Key" << endl;
+  cout << "key" << privateKeyResult.output->size() << endl;
   // sign Sha
   const sslHelper::result<vector<unsigned char>> sslSignatureStatus =
       sslHelper::getED25519Signature(*sslHashStatus.output,
