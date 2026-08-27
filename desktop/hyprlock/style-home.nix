069b5f39b4a8aa378cba2e48eb1925042381c4cd
Reese <r3es3y@gmail.com>
2026-08-28 11:49:50 +1200

    2026-08-28 - Attempting Build for gen: 98

--- desktop/hyprlock/style-home.nix
+++ desktop/hyprlock/style-home.nix
@@ -92,8 +92,8 @@
         outer_color = "rgba(${config.stylix.base16Scheme.base0D}ff)";
         check_color = "rgba(${config.stylix.base16Scheme.base08}ff)";
         fail_color = "rgba(${config.stylix.base16Scheme.base0D}ff) rgba(${config.stylix.base16Scheme.base0D}ff) 0deg";
-        inner_color = "rgba(${config.stylix.base16Scheme.base03}B0)";
-        font_color = "rgba(${config.stylix.base16Scheme.base05}ff)";
+        inner_color = "rgba(${config.stylix.base16Scheme.base05}B0)";
+        font_color = "rgba(${config.stylix.base16Scheme.base03}ff)";
         fade_on_empty = false;
         placeholder_text = "<i>Enter Password</i>";
         hide_input = false;
